ba9748cb570b00323d14bfd04fe9cf28e6276de5
Aydin1412 <162062700+Aydin1412@users.noreply.github.com>
2024-06-22 21:11:22 +0700

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -35,26 +35,21 @@ void printCWD(byte cwd) {}
 void parseCommand(char* buf, char* cmd, char arg[2][64]) {
   int i = 0, j = 0, k = 0;
 
-  // Parse command
   while (buf[i] != ' ' && buf[i] != '\0') {
     cmd[j++] = buf[i++];
   }
   cmd[j] = '\0';
 
-  // Skip spaces
   while (buf[i] == ' ') i++;
 
-  // Parse first argument
   j = 0;
   while (buf[i] != ' ' && buf[i] != '\0') {
     arg[0][j++] = buf[i++];
   }
   arg[0][j] = '\0';
 
-  // Skip spaces
   while (buf[i] == ' ') i++;
 
-  // Parse second argument
   j = 0;
   while (buf[i] != ' ' && buf[i] != '\0') {
     arg[1][j++] = buf[i++];
